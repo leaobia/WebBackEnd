96d6ca5e4bc76b58c615d1a6d635de2730ed85f5
unknown <biancapeleao@gmail.com>
2023-02-11 14:32:05 -0300

    separação de par e impar

--- AULA06EX03/modulo/calculo.js
+++ AULA06EX03/modulo/calculo.js
@@ -5,25 +5,74 @@
  * Versão: 1.0
  ******************************************************/
 
-const calculoParImpar = function (numeroInicial, numeroFinal) {
+const calculoParImpar = function (numeroInicial, numeroFinal, escolhaDeModo) {
+
     let valor1 = Number(String(numeroInicial).replace(',', '.'));
-    let valor2 = Number(String(numeroFinal).replace(',', '.'));
+  
+    let valor2 = Number(String(numeroFinal).replace(',','.'));
+  
+    let escolha = escolhaDeModo.toUpperCase();
+  
     let status = true;
-
+  
     if (valor1 == 0 || valor2 == 0)
-        status = false;
+  
+      status = false;
+  
     else if (isNaN(valor1) || isNaN(valor2))
-        status = false;
+  
+      status = false;
+  
     else {
+  
+      if(escolha == 'PAR' || escolha == 'AMBOS'){
+  
+        console.log('\nLista de Números Pares');
+  
+        let contadorPares = 0;
+  
         for (i = valor1; i <= valor2; i++) {
-            if (i % 2 == 0){
-                console.log(i,'par')
-            }else{
-                console.log(i,'impar')
-            }
+  
+          if (i % 2 == 0) { 
+  
+            console.log(i, 'par')
+  
+            contadorPares++
+  
+          }
+  
         }
+  
+        console.log('Quantidade de Pares encontrados: ' + contadorPares)
+  
+      }
+  
+      if(escolha == 'IMPAR' || escolha == 'AMBOS'){
+  
+        console.log('\nLista de Números Impares');
+  
+        let contadorImpares = 0;
+  
+        for (i = valor1; i <= valor2; i++) {
+  
+          if (i % 2 != 0) { 
+  
+            console.log(i, 'impar')
+  
+            contadorImpares++
+  
+          }
+  
+        }
+  
+        console.log('Quantidade de Impares encontrados: ' + contadorImpares)
+  
+      }
+  
     }
+  
     return status;
-}
-
-calculoParImpar(5,10)
+  
+  }
+  
+  calculoParImpar(10,5,'AMBOS');
