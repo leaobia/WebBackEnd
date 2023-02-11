5a8de01b3c0a01bfe1fab835bce9ae589ea13648
unknown <biancapeleao@gmail.com>
2023-02-11 20:30:28 -0300

    exercicio 02 finalizado

--- AULA06EX02/modulo/tabuadas.js
+++ AULA06EX02/modulo/tabuadas.js
@@ -13,17 +13,17 @@ const calcularTabuada = function (minMultiplicando, maxMultiplicando, minMultipl
     } else if (isNaN(tabuada1) || isNaN(maxContador)) {
         console.log('ERRO 02')
         status = false;
-    //} else if (tabuada1 > tabuada2 || minContador > maxContador) {
-       // console.log('ERRO 03')
-       // status = false;
+    } else if (tabuada1 > tabuada2 || minContador > maxContador) {
+        console.log('ERRO 03')
+        status = false;
     } else if (tabuada1 < 2 || tabuada1 > 50 || tabuada2 < 2 || tabuada2 > 50) {
         console.log('ERRO 04')
         status = false;
     } else {
         while (tabuada1 <= tabuada2) {
-            console.log('\n\nTabuada do: ' + tabuada1)
+            console.log('\n\nTabuada do: ' + tabuada1 + '\n')
             minMultiplicador2 = Number(minMultiplicador);
-            while (minMultiplicador2 < maxContador) {
+            while (minMultiplicador2 <= maxContador) {
                 resultado = tabuada1 * minMultiplicador2;
                 console.log(tabuada1 + 'x' + minMultiplicador2 + '=' + resultado);
                 minMultiplicador2++;
